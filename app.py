1a6fa38e093516c5228ba7fec8e7fc6b4f829134
badpoet <badpoet55@gmail.com>
2015-06-01 21:59:04 +0800

    Build app & transport script

---
+++ app.py
@@ -0,0 +1,50 @@
+__author__ = 'badpoet'
+
+import json
+import pymongo
+import pyhocon
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# FIXME keep token in secret
+
+conf = pyhocon.ConfigFactory.parse_file("resource/config.hocon")
+
+TOKEN = conf.get_string('app.token')
+db = pymongo.Connection(
+        host=conf.get_string('db.mongo.host'),
+        port=conf.get_int('db.mongo.port'),
+    )[conf.get_string('db.mongo.db')]
+# db.authenticate(
+#     conf.get_string('db.mongo.user'),
+#     conf.get_string('db.mongo.pw'),
+# )
+w_col = db["w"]
+
+@app.route("/data/<q_type>/<q_date>/<q_hour>", methods=["GET"])
+def query(q_type, q_date, q_hour):
+    token = request.args.get("token", "")
+    if not token == TOKEN:
+        res = {
+            "status": "error",
+            "err": "invalid token",
+            "result": []
+        }
+        return json.dumps(res, ensure_ascii=False)
+    result = w_col.find_one({
+        "type_key": q_type,
+        "date": q_date,
+        "hour": q_hour
+    })
+    if result == None:
+        res = {
+            "status": "none",
+            "result": []
+        }
+        return json.dumps(res, ensure_ascii=False)
+    res = {
+        "status": "ok",
+        "result": result
+    }
+    return json.dumps(res, ensure_ascii=False)
---
+++ scripts/transport.py
@@ -0,0 +1,62 @@
+__author__ = 'badpoet'
+
+import os
+import sys
+import pyhocon
+import pymongo
+
+conf = pyhocon.ConfigFactory.parse_file("resource/config.hocon")
+
+db = pymongo.Connection(
+        host=conf.get_string('db.mongo.host'),
+        port=conf.get_int('db.mongo.port'),
+    )[conf.get_string('db.mongo.db')]
+# db.authenticate(
+#     conf.get_string('db.mongo.user'),
+#     conf.get_string('db.mongo.pw'),
+# )
+w_col = db["w"]
+w_col.ensure_index([("type_key", 1), ("date", 1), ("hour, 1")])
+
+dir = sys.argv[1]
+if dir[:-1] != "/":
+    dir += '/'
+file_name_list = os.listdir(dir)
+
+
+def make_value(line, type_key):
+    tks = line.split()
+    lat = float(tks[0])
+    long = float(tks[1])
+    if type_key == "wind":
+        v = map(float, tks[2:])
+    elif type_key == "weather":
+        v = tks[2]
+    else:
+        v = float(tks[2])
+    return {
+        "lat": lat,
+        "long": long,
+        "value": v
+    }
+
+for each in file_name_list:
+    file_name = dir + each
+    print each
+    date = each[0:4]
+    hour = each[4:6]
+    type_key = each[7:]
+
+    f = open(file_name, "r")
+    values = [ make_value(line, type_key) for line in f.readlines() if not line ]
+
+    data_key = {
+        "type_key": type_key,
+        "date": date,
+        "hour": hour
+    }
+    w_col.update(
+        data_key,
+        { "$set": { "value": values }},
+        upsert=True
+    )

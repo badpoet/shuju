9ae505cc742a010faa882789fc95a43cc9e7b78e
badpoet <badpoet55@gmail.com>
2015-06-09 11:35:19 +0800

    add add date script

--- scripts/add_date.py
+++ scripts/add_date.py
@@ -10,7 +10,7 @@ conf = wlib.clients.get_conf()
 db = wlib.clients.mongo.connection(conf)
 
 date_bak = ""
-for each in db["raw"].find().sort({"timestamp": 1}):
+for each in db["raw"].find().sort([("timestamp", 1)]):
     mongo_id = each["_id"]
     date = each["timestamp"][:8]
     if not date == date_bak:
